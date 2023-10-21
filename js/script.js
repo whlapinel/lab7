e1df8de8a2dde9c710315b5d1bb59dc873cb13c3
whlapinel <140720302+whlapinel@users.noreply.github.com>
2023-10-20 20:58:07 -0400

    complete quiz7

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
 'use strict';
 
+const previewImg = document.querySelector('.preview-img');
+const gallery = document.querySelector('.gallery');
 
-    
+gallery.addEventListener('click', onClick);
+
+function onClick(event) {
+    if (event.target.tagName === 'IMG') {
+        previewImg.src = event.target.src;
+        previewImg.alt = event.target.alt;
+        console.log(previewImg.attributes);
+        document.querySelector('figcaption').textContent = previewImg.alt;
+    }
+}
 
 
 
